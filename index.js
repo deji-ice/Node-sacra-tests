4789bfc2a8b26329518cda97eccb4a7b22380824
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-11-28 12:35:21 +0100

    Refactor .gitignore and add .env file

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
 import express from "express";
+import userRoute from "./routes/userRoutes.js";
 
 const app = express();
 const port = 419;
-const users = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john@email.com",
-    age: 25,
-  },
-  {
-    id: 2,
-    name: "Jane Doe",
-    email: "jane@email.com",
-    age: 30,
-  },
-  {
-    id: 3,
-    name: "Jorja Smith",
-    email: "jorja@email.com",
-    age: 35,  
-  },
-]
+app.use(express.json()); // parse json request body
+
+app.use("/api/v1/users", userRoute) // use "/api/v1/users" for all user routes
 
 app.get("/", (req, res) => {
   res.send("hello welcome to my personal api server");
 });
 
-app.get("/users", (req, res) => {
-  res.json(users);
-});
-
-
 app.listen(port, () => {
-  console.log(`server is running on port http://localhost:${port}`);
+  console.log(`server is listening on port http://localhost:${port}`);
 });
