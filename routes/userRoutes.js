dda2f9b982ab4c5fab7a938739015284bc68a39d
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-11-28 12:37:27 +0100

    Refactor database connection and add user route

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import db from "../db/conn.js";
+const userRoute = express.Router();
+
+userRoute.get("/", async (req, res) => {
+  const collection = db.collection("users");
+  const result = await collection.find({}).limit(10).toArray();
+
+  res.status(200).json(result);
+});
+
+export default userRoute;
