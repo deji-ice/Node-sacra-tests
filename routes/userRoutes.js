6edd12ba83be9ef846c40613427f3e75c2a0cfa6
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-11-29 09:54:58 +0100

    Refactor userRoutes.js to remove unnecessary code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,6 @@ const userRoute = express.Router();
 userRoute.get("/", async (req, res) => {
   const collection = db.collection("users");
   const result = await collection.find({}).limit(10).toArray();
-
   res.status(200).json(result);
 });
 
