3ee91dff7e651310a6efec62ab4b8807cec17ec3
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-12-02 14:48:43 +0100

    Update userRoutes.js to change GET route to accept user ID and adjust error handling for user creation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,17 @@
 import express from "express";
-import User from "../models/userModel";
+import User from "../models/userModel.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/:id", (req, res) => {
   res.status(200).send("holla");
 });
 
 router.post("/create", async (req, res) => {
   try {
-    const { username } = req.body; // get the username from the request body
-    if (!username) {
-      return res.status(400).send("Username is required");
-    }
+    const { username } = req.body;
+    // get the username from the request body
+
     const existingUser = await User.findOne({ username }); // check if the user already exists
     if (existingUser) {
       return res.status(400).send("Username already exists");
@@ -21,7 +20,7 @@ router.post("/create", async (req, res) => {
     await user.save(); // save the user
     res.status(201).json("User created successfully");
   } catch (error) {
-    res.status(500).json({ message: error.message }); // send the error message
+    res.status(400).json({ message: error.message }); // send the error message
   }
 });
 
