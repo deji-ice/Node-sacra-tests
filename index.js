ff02b6ac7319cf0e8a02a9cd6233fb212ef9c300
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-12-13 12:26:07 +0100

    Add CORS middleware for cross-origin resource sharing

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dbConnection from "./db/conn.js";
 import userRoute from "./routes/userRoutes.js";
+import cors from "cors";
 
 const app = express();
 const port = 4000;
 
 dbConnection() // connect to the database
-
+app.use(cors())
 app.use(express.json()); // parse json request body
 app.use("/api/v1/users", userRoute); // use user router
 
